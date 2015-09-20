f1c79b7139f12317c9a5e211383bfd465ed02552
yuzutas0 <sho.y0921@gmail.com>
2015-09-20 20:54:21 +0900

    add method-for-execute-each-crawlers

--- lib/crawls/manager.rb
+++ lib/crawls/manager.rb
@@ -2,5 +2,9 @@
 class Crawls::Manager
   # rails runner Crawls::Manager.execute
   def self.execute
+    Crawls::Robots::Atnd.execute
+    Crawls::Robots::Doorkeeper.execute
+    Crawls::Robots::Zusaar.execute
+    Crawls::Robots::Connpass.execute
   end
 end
