a3f8b64f1e29f4b188a8a1367848af71508d79ff
yuzutas0 <sho.y0921@gmail.com>
2015-09-23 09:38:16 +0900

    practice react composing-component

--- app/assets/javascripts/components/event_box.js.jsx
+++ app/assets/javascripts/components/event_box.js.jsx
@@ -1,9 +1,30 @@
 var EventBox = React.createClass({
-
   render: function() {
     return(
       <div className="eventBox">
-        I am a EventBox!
+        <h1>Events</h1>
+        <EventList />
+        <EventForm />
+      </div>
+    );
+  }
+});
+
+var EventList = React.createClass({
+  render: function() {
+    return(
+      <div className="eventList">
+        I am a EventList!
+      </div>
+    );
+  }
+});
+
+var EventForm = React.createClass({
+  render: function() {
+    return(
+      <div className="eventForm">
+        I am a EventForm!
       </div>
     );
   }
